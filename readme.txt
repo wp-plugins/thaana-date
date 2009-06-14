5a7f746fbe62379d1bcb10f9d443445ecc18ad6c
fayax <fayax@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-06-14 12:49:51 +0000

    Adding thaana-date plugin
    git-svn-id: https://plugins.svn.wordpress.org/thaana-date/trunk@125768 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,17 @@
+=== Thaana Date ===
+Contributors: Fayax
+Donate linl: http://www.fayax.net/thaana-date
+Tags: date, pages, post
+Requires at least: 2.0.0
+Stable tag: trunk
+
+== Description ==
+
+This plugin replaces the english format of month and days in Wordpress to Thaana (Maldivian language) format.
+This plugin uses Unicode character to display date and month. You are required to use Unicode Thaana font and set the direction to rtl.
+
+== Installation ==
+
+1. Upload the `thaana_date` folder to `/wp-content/plugins/`
+2. Activate the plugin through the 'Plugins' menu in WordPress
+That's it! The plugin will replace the Wordpress date format to Thaana.
---
+++ thaana-date.php
@@ -0,0 +1,33 @@
+﻿<?php
+/*
+Plugin Name: Thaana Date
+Plugin URI: http://www.fayax.net/thaana-date
+Description: Converts english months and days to Thaana format.
+Version: 1.0
+Author: Fayax
+Author URI: http://www.fayax.net
+*/
+
+function thaana_date_replace($text) {
+	$days_english = array('Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday',
+	'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun', 
+	'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December', 
+	'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec', 
+	'1st', '2nd', '3rd', '4th', '5th', '6th', '7th', '8th', '9th', '0th');
+	
+	$days_thaana = array('ހޯމަ', 'އަންގާރަ', 'ބުދަ', 'ބުރާސްފަތި', 'ހުކުރު', 'ހޮނިހިރު', 'އާދިއްތަ', 
+	'ހޯމަ', 'އަންގާރް', 'ބުދަ', 'ބުރާސްފަތި', 'ހުކުރު', 'ހޮނިހިރު', 'އާދިއްތަ', 
+	'ޖެނުއަރީ', 'ފެބުރުއަރީ', 'މާޗް', 'އޭޕްރީލް', 'މެއި', 'ޖޫން', 'ޖުލައި', 'އޮގަސްޓް', 'ސެޕްޓެމްބަރު', 'އޮކްޓޯބަރ', 'ނޮވެމްބަރު', 'ޑިސެމްބަރު', 
+	'ޖެނުއަރީ', 'ފެބުރުއަރީ', 'މާޗް', 'އޭޕްރީލް', 'މެއި', 'ޖޫން', 'ޖުލައި', 'އޮގަސްޓް', 'ސެޕްޓެމްބަރު', 'އޮކްޓޯބަރު', 'ނޮވެމްބަރު', 'ޑިސެމްބަރު',
+	'1', '2', '3', '4', '5', '6', '7', '8', '9', '0');
+	$text = str_replace($days_english, $days_thaana, $text);
+	return $text;
+}
+
+add_filter('the_time', 'thaana_date_replace');
+add_filter('the_modified_time', 'thaana_date_replace');
+add_filter('the_date', 'thaana_date_replace');
+add_filter('the_modified_date', 'thaana_date_replace');
+add_filter('get_comment_date','thaana_date_replace');
+
+?>
